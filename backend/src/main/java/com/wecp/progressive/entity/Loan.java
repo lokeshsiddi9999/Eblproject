47dffd10926b8306f69c8347f44d00fbc133c891
username <wecp-dummy@gmail.com>
2024-01-27 11:27:55 +0000

    Day_1

--- backend/src/main/java/com/wecp/progressive/entity/Loan.java
+++ backend/src/main/java/com/wecp/progressive/entity/Loan.java
@@ -5,5 +5,33 @@ public class Loan {
     private Long id;
     private String loanType;
     private double amount;
+    public Loan() {
+    }
+    public Loan(Long id, String loanType, double amount) {
+        this.id = id;
+        this.loanType = loanType;
+        this.amount = amount;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getLoanType() {
+        return loanType;
+    }
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    
 
 }
